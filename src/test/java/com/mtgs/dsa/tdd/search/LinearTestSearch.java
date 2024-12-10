fb1845422880a63b6761b4c8b49e39bf3e3ab3b5
May Thagyan Soe <may.thagyan@privasia.com>
2024-12-10 08:40:26 +0800

    Linear Test Search

--- src/test/java/com/mtgs/dsa/tdd/search/LinearTestSearch.java
+++ src/test/java/com/mtgs/dsa/tdd/search/LinearTestSearch.java
@@ -1,8 +1,10 @@
 package com.mtgs.dsa.tdd.search;
 
-import com.mtgs.dsa.LinearSearch;
 import static junit.framework.TestCase.assertEquals;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.mtgs.dsa.LinearSearch;
 
 /**
  *
@@ -10,48 +12,50 @@ import org.junit.Test;
  */
 public class LinearTestSearch {
 
+    LinearSearch lsAlgo = new LinearSearch();
+    int[] input = {1, 2, 3, 4, 5, -1};
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "1,0",
+        "3,2",
+        "-1,5",
+        "120,-1",
+    })
+    void testSearchWithParameterizedTest(int item, int expected) {
+        int index = lsAlgo.search(input, item);
+        assertEquals(expected, index);
+    }
+
+    /*    
     @Test
     public void testBestCaseSearch() {
-        LinearSearch lsAlgo = new LinearSearch();
-        int[] input = {1, 2, 3, 4, 5};
-
         int index = lsAlgo.search(input, 1);
         assertEquals(0, index);
     }
 
     @Test
     public void testAverageCaseSearch() {
-        LinearSearch lsAlgo = new LinearSearch();
-        int[] input = {1, 2, 3, 4, 5};
-
         int index = lsAlgo.search(input, 3);
         assertEquals(2, index);
     }
 
     @Test
     public void testWorstCaseSearch() {
-        LinearSearch lsAlgo = new LinearSearch();
-        int[] input = {1, 2, 3, 4, 5};
-
         int index = lsAlgo.search(input, 5);
         assertEquals(4, index);
     }
 
     @Test
     public void testEmptyWorstCaseSearch() {
-        LinearSearch lsAlgo = new LinearSearch();
-        int[] input = {1, 2, 3, 4, 5};
-
-        int index = lsAlgo.search(input, -1);
+        int index = lsAlgo.search(input, -2);
         assertEquals(-1, index);
     }
 
     @Test
     public void testEdgeCaseSearch() {
-        LinearSearch lsAlgo = new LinearSearch();
-        int[] input = {1, 2, 3, 4, 5, -1};
-
         int index = lsAlgo.search(input, -1);
         assertEquals(5, index);
     }
+     */
 }
